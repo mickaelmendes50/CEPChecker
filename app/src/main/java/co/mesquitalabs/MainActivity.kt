63e1f99f3038d2a84ba92aae71397c88a96b06be
Mickael Mesquita <mesquita@aospa.co>
2026-02-23 21:40:13 -0300

    screens: Home: Implementada tela incial basica
    Signed-off-by: Mickael Mesquita <mesquita@aospa.co>

--- app/src/main/java/co/mesquitalabs/MainActivity.kt
+++ app/src/main/java/co/mesquitalabs/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import co.mesquitalabs.ui.screens.Home
 import co.mesquitalabs.ui.theme.CEPCheckerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CEPCheckerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Home(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CEPCheckerTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/co/mesquitalabs/ui/screens/Home.kt
+++ app/src/main/java/co/mesquitalabs/ui/screens/Home.kt
@@ -1,2 +1,95 @@
 package co.mesquitalabs.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {
+    val cep = remember { mutableStateOf("") }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .padding(top = 16.dp)
+
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box(
+               modifier = modifier
+                   .size(80.dp)
+                   .clip(shape = RoundedCornerShape(50))
+                   .background(MaterialTheme.colorScheme.error)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = null,
+                    modifier = modifier.size(180.dp),
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+
+            Text(
+                text = "Validador de CEP",
+                style = MaterialTheme.typography.headlineMedium,
+            )
+
+            OutlinedTextField(
+                value = cep.value,
+                onValueChange = { newValue ->
+                    val filteredText = newValue.filter {
+                        !it.isWhitespace()
+                        && it != '\n'
+                    }
+                    if (newValue.length <= 8)
+                        cep.value = filteredText
+                },
+                modifier = modifier.fillMaxWidth(),
+                label = { Text("CEP") },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                shape = RoundedCornerShape(50),
+            )
+
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.error,
+                )
+            ) {
+                Text(
+                    text = "Buscar",
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+        }
+    }
+}
